e89a31f0645bcce20150ab0bfdbe8edbb2d9b9af
Antonio Radosav <radosav.antonio@gmail.com>
2024-01-22 22:25:19 +0000

    final

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+import { getFontFamily } from './assets/getFontFamily'
 
 const App = (): JSX.Element => {
-  return <Text>DONATION_APP</Text>;
-};
+  return (
+    <SafeAreaView>
+      <Text
+        style={{ fontSize: 30, fontFamily: getFontFamily({ weight: '700' }) }}>
+        DONATION_APP
+      </Text>
+    </SafeAreaView>
+  )
+}
 
-export default App;
+export default App
